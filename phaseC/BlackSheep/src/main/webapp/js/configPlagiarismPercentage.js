fce31f4a14b77b288ed20062366eb351a8995e63
Athul Muralidharan <athulmurali@ccs.neu.edu>
2018-03-19 17:27:22 -0400

    #CS104-30 Added config percentage page

--- phaseC/BlackSheep/src/main/webapp/js/configPlagiarismPercentage.js
+++ phaseC/BlackSheep/src/main/webapp/js/configPlagiarismPercentage.js
@@ -1,9 +1,14 @@
-var elem = document.querySelector('input[type="range"]');
+window.onload  = function(){
+
+
+    var elem = document.getElementById('rangeSelector');
 
 var rangeValue = function(){
+    console.log("hi")
     var newValue = elem.value;
-    var target = document.querySelector('.value');
+    var target = document.getElementById("rangeValueDisplay")
     target.innerHTML = newValue;
 }
 
 elem.addEventListener("input", rangeValue);
+}
